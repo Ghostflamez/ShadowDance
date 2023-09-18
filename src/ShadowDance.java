7c40aea84bf6f6bff19d568a3cce02364e742b7d
hongjianz1 <hongjianz1@student.unimelb.edu.au>
2023-09-18 12:04:23 +1000

    Implemented the reading of CSV files and organized the data. Added error handling for reading invalid data.Implemented the transition from the start screen to the gameplay screen.

--- src/ShadowDance.java
+++ src/ShadowDance.java
@@ -1,4 +1,8 @@
 import bagel.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Skeleton Code for SWEN20003 Project 1, Semester 2, 2023
@@ -37,6 +41,76 @@ public class ShadowDance extends AbstractGame {
     //set messages options
     private final DrawOptions TITLE_OPTIONS = new DrawOptions().setBlendColour(1.0, 1.0, 1.0);
     private final DrawOptions INSTRUCTION_OPTIONS = new DrawOptions().setBlendColour(1.0, 1.0, 1.0);
+    private final Font SCORE_FONT = new Font("res/FSO8BITR.ttf", 35); // Assuming the font is located in the 'res' folder
+    private final double Y_POSITION = 384.0;
+
+    // Variables for the game state
+    private int score = 0; // The player's score
+
+    public ShadowDance() {
+        super(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE);
+        readCSV();
+    }
+
+    /**
+     * Method used to read file
+     * initialize the reference image
+     * store pop order of notes in an array
+     */
+    private ArrayList<String[]> refSet;
+    private ArrayList<String[]> popOrder;
+    private boolean isLaneType(String value) { // check if the value is a lane type
+        return "Left".equals(value) || "Right".equals(value) || "Up".equals(value) || "Down".equals(value);
+    }
+    private void readCSV() {
+        refSet = new ArrayList<>();
+        popOrder = new ArrayList<>();
+        String path = "G:\\SWEN_project1\\hongjianz1-project-1\\res\\test1.csv"; // find the path of the file
+        int lineCount = 0;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            // Step 1: Preliminary check
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data.length != 3) {
+                    throw new IOException("Invalid number of columns on line " + lineCount);
+                }
+                lineCount++;
+            }
+
+            if (lineCount < 4) {
+                throw new IOException("Insufficient number of lines in the CSV. Expected at least 4 but got " + lineCount);
+            }
+
+            // Resetting the BufferedReader to read the file again
+            br.close();
+            br = new BufferedReader(new FileReader(path));
+            lineCount = 0;
+
+            // Step 2: Process the data
+            while ((line = br.readLine()) != null) {
+                lineCount++;
+                String[] data = line.split(",");
+
+                if ("Lane".equals(data[0])) {
+                    String refType = data[1];
+                    double refxCoordinate = Double.parseDouble(data[2].trim());
+                    refSet.add(new String[]{refType, String.valueOf(refxCoordinate), "lane" + refType + ".png"});
+                } else if (isLaneType(data[0])) {
+                    popOrder.add(data);
+                } else {
+                    throw new IOException("Unexpected value in the first column on line " + lineCount);
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     //draw initial interface
     private void drawInitialInterface(){
@@ -48,23 +122,23 @@ public class ShadowDance extends AbstractGame {
         INSTRUCTION_FONT.drawString(INSTRUCTION2, 320.0, 490.0, INSTRUCTION_OPTIONS);
     }
 
-    public ShadowDance() {
-        super(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE);
-    }
+    private void drawPlayingInterface() {
+        // Drawing the score
+        SCORE_FONT.drawString("SCORE  " + score, 20, 50); // 10, 10 为左上角坐标
 
-    /**
-     * Method used to read file and create objects (you can change
-     * this method as you wish).
-     */
-    private void readCSV() {
-
+        // Drawing images based on refSet data
+        for (String[] data : refSet) {
+            double xPosition = Double.parseDouble(data[1]); // Convert the string to a double
+            String imageName = data[2]; // Get the image name
+            Image img = new Image("res/" + imageName); // load the image
+            img.draw(xPosition, Y_POSITION);
+        }
     }
 
     /**
      * The entry point for the program.
      */
     public static void main(String[] args) {
-
         ShadowDance game = new ShadowDance();
         game.run();
     }
@@ -98,6 +172,7 @@ public class ShadowDance extends AbstractGame {
             drawInitialInterface();
         }
         else {
+            drawPlayingInterface();
             if (input.isDown(Keys.LEFT)) {
                 // Add logical judgment statement
             }
